b7fc7a033b2d6fea0bb0c5d06963a6255c5740e2
tcjansen <jansent@astro.columbia.edu>
2019-07-02 11:32:48 -0400

    Fixed the docstring to match astropy formatting

--- snr.py
+++ snr.py
@@ -2,39 +2,41 @@ import numpy as np
 import astropy.units as u
 
 
-def snr(counts, npix=1, nb=np.inf, background=0, darkcurrent=0,
-        readnoise=0, gain=0, ADerr=0.289):
-    """ A function to calculate the signal to noise ratio (SNR) of an
-    astronomical observation.
+def snr(counts, npix=1, n_background=np.inf, background=0, darkcurrent=0,
+        readnoise=0, gain=0, ad_err=0.289):
+    """
+    A function to calculate the signal to noise ratio (SNR) of an
+    astronomical observation (from "Handbook of CCD Astronomy",
+    Steve Howell, 2000, pg 55).
 
     Parameters
     ----------
-    counts (float or int)
+    counts : float or int
         Total number of counts in an arbitrary exposure time.
-    npix (int)
+    npix : float or int, optional
         Number of pixels under consideration for the signal. Default is 1.
-    nb (int)
+    n_background : float or int, optional
         Number of pixels used in the background estimation. Default
         is set to infinite such that there is no contribution of
         error due to background estimation. This assumes that the
-        nb will be >> npix.
-    background (int)
+        n_background will be >> npix.
+    background : float or int, optional
         Total photons per pixel due to the backround/sky. Default is 0.
-    darkcurrent (int)
+    darkcurrent : float or int, optional
         Total electrons per pixel due to the dark current. Default is 0.
-    readnoise (int)
+    readnoise : float or int, optional
         Electrons per pixel from the read noise. Default is 0.
-    gain (int)
+    gain : float or int, optional
         Gain of the CCD in electrions/ADU. Default is 0 such that the
         contribution to the error due to the gain is assumed to be small.
-    ADerr (float)
+    ad_err : float or int, optional
         An estimate of the 1 sigma error within the A/D converter.
         Default is set to 0.289 (Merline & Howell, 1995).
 
     Returns
     -------
-    float
-    The signal to noise ratio of the given observation in sqrt(e-).
+    sn : float
+        The signal to noise ratio of the given observation in sqrt(e-).
     """
     args = locals()  # maps arguments into a dictionary
 
@@ -44,14 +46,16 @@ def snr(counts, npix=1, nb=np.inf, background=0, darkcurrent=0,
             args[arg] = args[arg] * get_unit(arg)
 
     readnoise = get_shotnoise(args['readnoise'])
-    gain_err = get_shotnoise((args['gain'] * args['ADerr']))
+    gain_err = get_shotnoise((args['gain'] * args['ad_err']))
 
-    pixel_terms = args['npix'] * (1 + args['npix'] / args['nb'])
+    pixel_terms = args['npix'] * (1 + args['npix'] / args['n_background'])
     detector_noise = args['background'] + args['darkcurrent'] + \
         readnoise ** 2 + gain_err ** 2
 
-    return args['counts'] / np.sqrt(args['counts'] +
-                                   pixel_terms * detector_noise)
+    sn = args['counts'] / np.sqrt(args['counts'] +
+                                  pixel_terms * detector_noise)
+
+    return sn
 
 
 def get_unit(arg):
@@ -59,13 +63,13 @@ def get_unit(arg):
     """
     if arg == 'counts':
         return u.electron
-    elif arg in ['npix', 'nb']:
+    elif arg in ['npix', 'n_background']:
         return u.pixel
     elif arg in ['background', 'darkcurrent', 'readnoise']:
         return u.electron / u.pixel
     elif arg == 'gain':
         return u.electron / u.adu
-    elif arg == 'ADerr':
+    elif arg == 'ad_err':
         return u.adu / u.pixel
 
 
@@ -87,13 +91,13 @@ def test_units():
     darkcurrent = 22 * t / 60 / 60
     gain = 5
     background = 620 * gain
-    nb = 200
+    n_background = 200
     npix = 1
     counts = 24013 * gain
 
     result = snr(counts, npix=npix, background=background,
                  darkcurrent=darkcurrent, readnoise=readnoise,
-                 gain=gain, nb=nb)
+                 gain=gain, n_background=n_background)
 
     # the value of the answer given in the text
     answer = 342 * np.sqrt(1 * u.electron)
@@ -113,13 +117,13 @@ def test_math():
     darkcurrent = (darkcurrent * t).to(u.electron / u.pixel)
     gain = 5 * u.electron / u.adu
     background = 620 * u.adu / u.pixel * gain
-    nb = 200 * u.pixel
+    n_background = 200 * u.pixel
     npix = 1 * u.pixel
     counts = 24013 * u.adu * gain
 
     result = snr(counts, npix=npix, background=background,
                  darkcurrent=darkcurrent, readnoise=readnoise,
-                 gain=gain, nb=nb)
+                 gain=gain, n_background=n_background)
 
     # the value of the answer given in the text
     answer = 342 * np.sqrt(1 * u.electron)
